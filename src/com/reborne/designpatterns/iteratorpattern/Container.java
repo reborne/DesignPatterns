772dfbf2f2a28c5e979ca26f253b210932c0db73
reborne <artyom.u@outlook.com>
2016-03-27 15:43:01 +0300

    Revert "Added Interpreter Pattern"
    This reverts commit 7e4c50ac1d8cf2648f1c379192fffabb2ccbe0fd.
    
    Reverting commit with wrong naming "Added Interpreter Pattern"

--- src/com/reborne/designpatterns/iteratorpattern/Container.java
+++
@@ -1,5 +0,0 @@
-package com.reborne.designpatterns.iteratorpattern;
-
-public interface Container {
-	public Iterator getIterator();
-}
--- src/com/reborne/designpatterns/iteratorpattern/Iterator.java
+++
@@ -1,7 +0,0 @@
-package com.reborne.designpatterns.iteratorpattern;
-
-public interface Iterator {
-	public boolean hasNext();
-
-	public Object next();
-}
