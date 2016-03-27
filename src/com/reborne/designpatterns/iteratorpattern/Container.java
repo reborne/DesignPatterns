7e4c50ac1d8cf2648f1c379192fffabb2ccbe0fd
reborne <artyom.u@outlook.com>
2016-03-27 13:14:34 +0300

    Added Interpreter Pattern

---
+++ src/com/reborne/designpatterns/iteratorpattern/Container.java
@@ -0,0 +1,5 @@
+package com.reborne.designpatterns.iteratorpattern;
+
+public interface Container {
+	public Iterator getIterator();
+}
---
+++ src/com/reborne/designpatterns/iteratorpattern/IteratorPatternDemo.java
@@ -0,0 +1,22 @@
+package com.reborne.designpatterns.iteratorpattern;
+
+/*
+ * Iterator pattern is a commonly used design pattern in Java and .Net programming
+ * environment. This pattern is used to get a way to access the elements of a
+ * collection object in sequential manner without any need to know its underlying
+ * representation.
+ * Iterator pattern falls under behavioral pattern category. 
+ */
+
+public class IteratorPatternDemo {
+	
+	public static void main(String[] args) {
+		NamedRepository namesRepository = new NamedRepository();
+		
+		for(Iterator iter = namesRepository.getIterator();iter.hasNext();){
+			String name = (String) iter.next();
+			System.out.println("Name: " + name);
+		}
+	}
+	
+}
