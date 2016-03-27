43b8ea184cb43c17c6e6a34da657d5f73beb7dd1
reborne <artyom.u@outlook.com>
2016-03-27 12:58:49 +0300

    Added Interpreter Pattern

---
+++ src/com/reborne/designpatterns/interpreterpattern/AndExpression.java
@@ -0,0 +1,21 @@
+package com.reborne.designpatterns.interpreterpattern;
+
+public class AndExpression implements Expression{
+
+	private Expression exp1 = null;
+	private Expression exp2 = null;	
+	
+	
+	public AndExpression(Expression exp1, Expression exp2) {
+		this.exp1 = exp1;
+		this.exp2 = exp2;
+	}
+
+
+	@Override
+	public boolean interpret(String context) {
+		// TODO Auto-generated method stub
+		return exp1.interpret(context) && exp2.interpret(context);
+	}
+	
+}
---
+++ src/com/reborne/designpatterns/interpreterpattern/Expression.java
@@ -0,0 +1,5 @@
+package com.reborne.designpatterns.interpreterpattern;
+
+public interface Expression {
+	public boolean interpret(String context);
+}
